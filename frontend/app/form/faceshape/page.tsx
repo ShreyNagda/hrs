a68df6840fdce5a33e911c5074d47bf032acc1bb
Shrey Nagda <shreynagda2714@gmail.com>
2025-04-23 13:25:52 +0530

    env changes

--- frontend/app/form/faceshape/page.tsx
+++ frontend/app/form/faceshape/page.tsx
@@ -2,7 +2,7 @@
 import React, { useRef, useState } from "react";
 import { useFormContext } from "@/app/context/FormContext";
 import { toast } from "react-toastify";
-import api from "@/app/axios";
+// import api from "@/app/axios";
 import Image from "next/image";
 import axios from "axios";
 
